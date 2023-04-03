2a0bc45ef9ab711c3df0f3644804a37038839231
Jenny Carroll <jennygc@telus.net>
2023-04-03 23:56:52 +0000

    complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+totalRevenue = Store.all.sum(:annual_revenue)
+puts totalRevenue
+
+averageRevenue = totalRevenue/Store.count
+puts averageRevenue
+
+largeAnnualSales = Store.where('annual_revenue >= 1000000')
+
+puts largeAnnualSales.sum(:annual_revenue)
